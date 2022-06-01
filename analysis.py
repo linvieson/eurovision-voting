cc2960206cb517b4afcba7a0c976e0a3e44d8c63
Nastya <73235121+be-unkind@users.noreply.github.com>
2022-06-01 18:50:22 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -14,46 +14,36 @@ from process_data import extract_participants
 
 # winners table
 winners_data = pd.read_csv('eurovision_winners.csv')
-# print(winners_data.head()) 
 
 # random sampling of the year the winner of this year (some country)
-sample = winners_data.sample()
-
+# sample = winners_data.sample()
 # YEAR = sample.iat[0,0]
 # COUNTRY = sample.iat[0,1]
 
 # example
 YEAR = '2015'
 COUNTRY = 'Sweden'
+LANGUAGE = 'english'
 
-# get votes for COUNTRY
-for_country_dict = get_votes_from_country(str(COUNTRY.lower()), 'televoters', str(YEAR))
+# get votes for COUNTRY in the chosen YEAR
+this_year = get_votes_from_country(str(COUNTRY.lower()), 'televoters', str(YEAR))
 
-votes = pd.DataFrame(list(for_country_dict.items()), \
-                    index = [x for x in range(len(for_country_dict.items()))], \
-                    columns = ['country', 'votes'])
-votes['votes'] = votes['votes'].astype(int)
-# print(votes)
+votes_this = pd.DataFrame(list(this_year.items()), \
+                    index = [x for x in range(len(this_year.items()))], \
+                    columns = ['country', 'votes_this'])
+votes_this['votes_this'] = votes_this['votes_this'].astype(int)
 
-votes_list = []
-for year_number in range(int(YEAR) - 5, int(YEAR)):
-    country_dict = get_votes_from_country(str(COUNTRY.lower()), 'televoters', str(year_number))
-    # print(country_dict)
-    if len(country_dict) != 0:
-        votes_year = pd.DataFrame(list(country_dict.items()), \
-                        index = [x for x in range(len(country_dict.items()))], \
-                        columns = ['country', f'votes{year_number}'])
-        # print(votes_year)
-        votes_year[f'votes{year_number}'] = votes_year[f'votes{year_number}'].astype(int)
-        votes = pd.concat([votes, votes_year])
-        # votes = votes.groupby(['country']).sum()
-        # votes = pd.merge(votes, votes_year, on=['country'])
-        # votes_list.append(votes_year)
+# get votes for COUNTRY in the previous year (YEAR - 1)
+previous = get_votes_from_country(str(COUNTRY.lower()), 'televoters', str(int(YEAR) - 1))
 
-# votes_df = votes_list[0].merge(votes_list[1], on=['country']).merge(votes_list[2], on=['country']).merge(votes_list[3], on=['country'])
-# votes.df = votes.merge(votes_df, on=['country'])
+votes_previous = pd.DataFrame(list(previous.items()), \
+                    index = [x for x in range(len(previous.items()))], \
+                    columns = ['country', 'votes_previous'])
+votes_previous['votes_previous'] = votes_previous['votes_previous'].astype(int)
 
-votes_df = votes.fillna(0)
+# join series into one dataframe with votes
+votes = pd.concat([votes_this, votes_previous])
+votes = votes.groupby(['country']).sum().reset_index()
 
 # get migrants from this COUNTRY in chosen YEAR 
 participants = extract_participants('ev_all_votes.csv')
@@ -61,52 +51,96 @@ df = pd.read_csv('migrants.csv', dtype=str)
 df = clean_data(participants, df)
 
 # migrants in YEAR
-migrants_from = get_migrants_from_country(df, str(COUNTRY.lower()), str(int(YEAR) - 1))
+migrants_from = get_migrants_from_country(df, str(COUNTRY.lower()), str(YEAR))
 
 migrants = pd.DataFrame(list(migrants_from.items()), \
                         index = [x for x in range(len(migrants_from.items()))], \
                         columns = ['country', 'migrants'])
-migrants[f'migrants{int(YEAR) - 1}'] = migrants['migrants'].apply(lambda x: x.replace(',', '')).astype(int)
+migrants[f'migrants{YEAR}'] = migrants['migrants'].apply(lambda x: x.replace(',', '')).astype(int)
 
-# migrants in 4 previous years
-# migrants_list = []
-# for year_number in range(int(YEAR) - 4, int(YEAR)):
-#     migrants_dict = get_migrants_from_country(df, str(COUNTRY.lower()), str(year_number))
-#     # print(country_dict)
-#     if len(migrants_dict) != 0:
-#         migrants_year = pd.DataFrame(list(migrants_dict.items()), \
-#                         index = [x for x in range(len(migrants_dict.items()))], \
-#                         columns = ['country', f'migrants{year_number}'])
-#         # print(votes_year)
-#         migrants_year[f'migrants{year_number}'] = migrants_year[f'migrants{year_number}'].apply(lambda x: x.replace(',', '')).astype(int)
-#         # votes = pd.concat([votes, votes_year])
-#         # votes = votes.groupby(['country']).sum()
-#         # votes = pd.merge(votes, votes_year, on=['country'])
-#         migrants_list.append(migrants_year)
+# migrants in previous years
+migrants_list = []
+for year_number in range(1990, int(YEAR) + 1):
+    migrants_dict = get_migrants_from_country(df, str(COUNTRY.lower()), str(year_number))
+    # print(country_dict)
+    if len(migrants_dict) != 0:
+        migrants_year = pd.DataFrame(list(migrants_dict.items()), \
+                        index = [x for x in range(len(migrants_dict.items()))], \
+                        columns = ['country', f'migrants{year_number}'])
+        # print(votes_year)
+        migrants_year[f'migrants{year_number}'] = migrants_year[f'migrants{year_number}'].apply(lambda x: x.replace(',', '')).astype(int)
+        # votes = pd.concat([votes, votes_year])
+        # votes = votes.groupby(['country']).sum()
+        # votes = pd.merge(votes, votes_year, on=['country'])
+        migrants_list.append(migrants_year)
 
-# migrants_df = migrants_list[0].merge(migrants_list[1], on=['country']).merge(migrants_list[2], on=['country']).merge(migrants_list[3], on=['country'])
-# print(migrants_df)
-
+migrants_df = migrants_list[0].merge(migrants_list[1], on=['country'])
+for year_num in range(2, int(YEAR) - 1990):
+    migrants_df = migrants_df.merge(migrants_list[year_num], on=['country'])
 
 # create dataframe with this COUNTRYs borders
 borders = pd.read_csv('borders.csv')
-
 country_borders = pd.DataFrame()
 country_borders['country'] = borders['Country']
 country_borders['borders'] = borders[str(COUNTRY).lower()].astype(int)
 
+# dataframe for languages match
+language = pd.DataFrame()
+language['country'] = pd.read_csv('borders.csv')['Country']
+language['lang'] = np.where(pd.read_csv('borders.csv')['official_language'] == LANGUAGE, 1, 0)
+
+
+# Regression by migrants  
+
 # merge votes, migrants and borders into one dataframe
-# votes_merged = pd.merge(votes, votes_df, on=['country'])
-votes_migrants = pd.merge(votes_df, migrants, on=['country'])
+votes_migrants = pd.merge(votes, migrants_df, on=['country'])
 country_merged = votes_migrants.merge(country_borders, on=['country'])
-print(country_merged)
+merged_df = country_merged.merge(language, on=['country'])
+print(merged_df)
+
+str_formula = f'votes_previous ~ borders + lang +'
+for year in range(1990, int(YEAR) - 1):
+    str_formula = str_formula + f'migrants{year} + '
+str_formula = str_formula[:-2]
 
 # model the Ordinary Least Squares regression
-model = smf.ols(formula = f'votes{int(YEAR) - 1} ~ votes{int(YEAR) - 2} + votes{int(YEAR) - 3} + votes{int(YEAR) - 4} \
-                            + migrants{int(YEAR) - 1} + borders', data=country_merged.dropna())
+model = smf.ols(formula = str_formula, data=merged_df.dropna())
 model = model.fit()
 print(model.summary())
 
 
+# Regression by votes
+votes_list = []
+for year_number in range(1990, int(YEAR)):
+    country_dict = get_votes_from_country(str(COUNTRY.lower()), 'televoters', str(year_number))
+    # print(country_dict)
+    if len(country_dict) != 0:
+        votes_year = pd.DataFrame(list(country_dict.items()), \
+                        index = [x for x in range(len(country_dict.items()))], \
+                        columns = ['country', f'votes{year_number}'])
+        votes_year[f'votes{year_number}'] = votes_year[f'votes{year_number}'].astype(int)
+        votes_list.append(votes_year)
 
-# {int(YEAR) - 1} + migrants{int(YEAR) - 2} + migrants{int(YEAR) - 3} + migrants{int(YEAR) - 4}
+# dataframe with votes from 1990 to the YEAR
+votes_df = pd.concat([votes_list[0], votes_list[1]])
+for year_num in range(2, int(YEAR) - 1990 - 1):
+    votes_df = pd.concat([votes_df, votes_list[year_num]])
+    votes_df = votes_df.groupby(['country']).sum().reset_index()
+
+votes_df = votes_df.merge(votes, on=['country'])
+
+# merge needed dataframes
+country_merged = votes_df.merge(country_borders, on=['country'])
+merged_df = country_merged.merge(language, on=['country'])
+print(merged_df)
+
+str_formula = f'votes_previous ~ borders + lang +'
+for year in range(1990, int(YEAR) - 1):
+    str_formula = str_formula + f'votes{year} + '
+str_formula = str_formula[:-2]
+str_formula = str_formula.replace('votes2010 +', '')
+
+# model the Ordinary Least Squares regression
+model_2 = smf.ols(formula = str_formula, data=merged_df.dropna())
+model_2 = model_2.fit()
+print(model_2.summary())
