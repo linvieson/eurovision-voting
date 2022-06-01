739324749d457bb1c0ad1e17e9c001701886c056
Nastya <73235121+be-unkind@users.noreply.github.com>
2022-06-01 12:36:22 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -61,31 +61,31 @@ df = pd.read_csv('migrants.csv', dtype=str)
 df = clean_data(participants, df)
 
 # migrants in YEAR
-migrants_from = get_migrants_from_country(df, str(COUNTRY.lower()), str(YEAR))
+migrants_from = get_migrants_from_country(df, str(COUNTRY.lower()), str(int(YEAR) - 1))
 
 migrants = pd.DataFrame(list(migrants_from.items()), \
                         index = [x for x in range(len(migrants_from.items()))], \
                         columns = ['country', 'migrants'])
-migrants['migrants'] = migrants['migrants'].apply(lambda x: x.replace(',', '')).astype(int)
+migrants[f'migrants{int(YEAR) - 1}'] = migrants['migrants'].apply(lambda x: x.replace(',', '')).astype(int)
 
 # migrants in 4 previous years
-migrants_list = []
-for year_number in range(int(YEAR) - 4, int(YEAR)):
-    migrants_dict = get_migrants_from_country(df, str(COUNTRY.lower()), str(year_number))
-    # print(country_dict)
-    if len(migrants_dict) != 0:
-        migrants_year = pd.DataFrame(list(migrants_dict.items()), \
-                        index = [x for x in range(len(migrants_dict.items()))], \
-                        columns = ['country', f'migrants{year_number}'])
-        # print(votes_year)
-        migrants_year[f'migrants{year_number}'] = migrants_year[f'migrants{year_number}'].apply(lambda x: x.replace(',', '')).astype(int)
-        # votes = pd.concat([votes, votes_year])
-        # votes = votes.groupby(['country']).sum()
-        # votes = pd.merge(votes, votes_year, on=['country'])
-        migrants_list.append(migrants_year)
+# migrants_list = []
+# for year_number in range(int(YEAR) - 4, int(YEAR)):
+#     migrants_dict = get_migrants_from_country(df, str(COUNTRY.lower()), str(year_number))
+#     # print(country_dict)
+#     if len(migrants_dict) != 0:
+#         migrants_year = pd.DataFrame(list(migrants_dict.items()), \
+#                         index = [x for x in range(len(migrants_dict.items()))], \
+#                         columns = ['country', f'migrants{year_number}'])
+#         # print(votes_year)
+#         migrants_year[f'migrants{year_number}'] = migrants_year[f'migrants{year_number}'].apply(lambda x: x.replace(',', '')).astype(int)
+#         # votes = pd.concat([votes, votes_year])
+#         # votes = votes.groupby(['country']).sum()
+#         # votes = pd.merge(votes, votes_year, on=['country'])
+#         migrants_list.append(migrants_year)
 
-migrants_df = migrants_list[0].merge(migrants_list[1], on=['country']).merge(migrants_list[2], on=['country']).merge(migrants_list[3], on=['country'])
-print(migrants_df)
+# migrants_df = migrants_list[0].merge(migrants_list[1], on=['country']).merge(migrants_list[2], on=['country']).merge(migrants_list[3], on=['country'])
+# print(migrants_df)
 
 
 # create dataframe with this COUNTRYs borders
@@ -97,16 +97,16 @@ country_borders['borders'] = borders[str(COUNTRY).lower()].astype(int)
 
 # merge votes, migrants and borders into one dataframe
 # votes_merged = pd.merge(votes, votes_df, on=['country'])
-votes_migrants = pd.merge(votes_df, migrants_df, on=['country'])
+votes_migrants = pd.merge(votes_df, migrants, on=['country'])
 country_merged = votes_migrants.merge(country_borders, on=['country'])
 print(country_merged)
 
 # model the Ordinary Least Squares regression
 model = smf.ols(formula = f'votes{int(YEAR) - 1} ~ votes{int(YEAR) - 2} + votes{int(YEAR) - 3} + votes{int(YEAR) - 4} \
-                            + migrants{int(YEAR) - 1} + migrants{int(YEAR) - 2} + migrants{int(YEAR) - 3} + migrants{int(YEAR) - 4}\
-                            + borders', data=country_merged.dropna())
+                            + migrants{int(YEAR) - 1} + borders', data=country_merged.dropna())
 model = model.fit()
 print(model.summary())
 
 
 
+# {int(YEAR) - 1} + migrants{int(YEAR) - 2} + migrants{int(YEAR) - 3} + migrants{int(YEAR) - 4}
